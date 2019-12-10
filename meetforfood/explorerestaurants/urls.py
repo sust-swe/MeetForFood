b7432bc499506d4e43c0b09efdab9c7e10800dec
waqil52 <waqilshopnil@gmail.com>
2019-12-11 00:14:25 +0600

    Filtering done

--- meetforfood/explorerestaurants/urls.py
+++ meetforfood/explorerestaurants/urls.py
@@ -4,14 +4,15 @@ from rest_framework.routers import DefaultRouter
 from explorerestaurants import views
 
 routers = DefaultRouter()
-routers.register('restaurants',views.RestaurantInfoViewSet)
-routers.register('menu',views.MenuInfoViewSet)
+routers.register('restaurant',views.RestaurantInfoViewSet)
+# routers.register('menu',views.MenuInfoViewSet)
 
 
 
 urlpatterns = [
     path('',include(routers.urls)),
-    path('^menu/(?P<restaurantid>.+)/$', views.MenuInfoViewSet.as_view({'get': 'list'})),
+    # path('restaurants/(?P<id>[0-9]+)/menu/', views.MenuInfoViewSet)
+    path('restaurants/<int:pk>/menu/', views.RestaurantMenuView.as_view()),
 
 
 ] + routers.urls
