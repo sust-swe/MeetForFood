9c0ac5f18a182bd5f79dcf3e16fe8e8a6cc88e6d
waqil52 <waqilshopnil@gmail.com>
2019-12-16 14:20:49 +0600

    defining model and consumer for chat

--- meetforfood/chat/models.py
+++ meetforfood/chat/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+# from profiles.models import UserProfile as User
+import uuid
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
+
+def validate_message_content(content):
+    if content is None or content == "" or content.isspace():
+        raise ValidationError(
+            'Content is empty or invalid',
+            code = 'invalid',
+            params = {
+                'content' : content
+            }
+        )
+        
+class User(AbstractUser):
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+        
+class Message(models.Model):
+    id = models.UUIDField(primary_key = True,null = False, default = uuid.uuid4, editable = False)
+    author = models.ForeignKey(User, blank = False, null = False, related_name='author_messages', on_delete=models.CASCADE)
+    content = models.TextField(validators=[validate_message_content])
+    created_at = models.DateTimeField(auto_now_add = True, blank = True)
+
+ 
+
 # Create your models here.
