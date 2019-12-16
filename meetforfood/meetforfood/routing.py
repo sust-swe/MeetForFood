8ef9a781485cf6b5fb7990e04b91c040d7a26454
waqil52 <waqilshopnil@gmail.com>
2019-12-16 12:19:47 +0600

    Initialized asgi and routing in Django

---
+++ meetforfood/meetforfood/routing.py
@@ -0,0 +1,17 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import re_path
+from chat import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat$', consumers.ChatConsumer),
+]
+
+application = ProtocolTypeRouter({
+    # http is channels.http.AsgiHandler by default 
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
